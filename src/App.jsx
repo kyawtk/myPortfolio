259d486c347ff2269716b6dbd7f35e5385cbcb91
kyawtk <kyawthantkhaing@gmail.com>
2023-07-04 20:04:47 +0630

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-
-import './App.scss'
-import {Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Footer from './components/Footer'
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Footer from "./components/Footer";
 function App() {
- return(
-  <>
-  <Routes>
-    <Route path='/' element={<Home/>}></Route>
-  </Routes>
-  <Footer></Footer>
-</>
- )
-
-  
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
+  );
 }
 
-export default App
+export default App;
