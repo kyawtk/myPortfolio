72785665ad8cb88e24645866e224f15590956c07
kyawtk <kyawthantkhaing@gmail.com>
2023-07-05 11:16:10 +0630

    added project

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
+import { ParallaxProvider } from 'react-scroll-parallax';
+
 function App() {
   return (
     <>
+    <ParallaxProvider>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
       </Routes>
       <Footer></Footer>
+      </ParallaxProvider>
     </>
   );
 }
